a58b322b3eec7b2e1ca94a9ae2313e7ef05501fe
already-amir <already.amirm@gmail.com>
2025-11-09 21:52:24 +0330

    k

---
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_apptest_1_FILE C:/Users/alrea/Desktop/test/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/apptest_1.exe)
+set(__QT_DEPLOY_TARGET_apptest_1_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_apptest_1_RUNTIME_DLLS G:/DEV/Qt/6.9.1/mingw_64/bin/Qt6Quick.dll;G:/DEV/Qt/6.9.1/mingw_64/bin/Qt6QmlMeta.dll;G:/DEV/Qt/6.9.1/mingw_64/bin/Qt6QmlWorkerScript.dll;G:/DEV/Qt/6.9.1/mingw_64/bin/Qt6QmlModels.dll;G:/DEV/Qt/6.9.1/mingw_64/bin/Qt6Qml.dll;G:/DEV/Qt/6.9.1/mingw_64/bin/Qt6Network.dll;G:/DEV/Qt/6.9.1/mingw_64/bin/Qt6OpenGL.dll;G:/DEV/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;G:/DEV/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
---
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/bin/qt_setup_tool_path.bat
@@ -0,0 +1,3 @@
+@echo off
+set PATH=G:\DEV\Qt\6.9.1\mingw_64\bin;%PATH%
+%*
