79058fcae3b4c6cae296879d4811072d1d284272
Amir <already.amirm@gmail.com>
2025-12-02 10:55:26 +0330

    2/12/2025 10:55

--- project1/res/control/bluetooth.h
+++ project1/res/control/bluetooth.h
@@ -8,6 +8,8 @@ class Bluetooth : public QObject
     Q_OBJECT
     Q_PROPERTY(bool bluetooth_enabeld READ bluetooth_enabeld WRITE setBluetooth_enabeld NOTIFY bluetooth_enabeldChanged FINAL)
     Q_PROPERTY(QStringListModel out READ out WRITE setout NOTIFY outChanged FINAL)
+    Q_PROPERTY(QString connected_bluetooth READ connected_bluetooth WRITE setConnected_bluetooth NOTIFY connected_bluetoothChanged FINAL)
+
 public:
     explicit Bluetooth(QObject *parent = nullptr);
 
